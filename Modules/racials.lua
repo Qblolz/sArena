9d9326c0a3800f3240aca1443fd771eca308b0d1
Qblolz <doublemageinc@gmail.com>
2021-12-03 21:11:14 +0300

    Update racials.lua

--- Modules/racials.lua
+++ Modules/racials.lua
@@ -187,7 +187,7 @@ end
 
 function module:OnEvent(event, ...)
 	for i = 1, MAX_ARENA_ENEMIES do
-		local CC
+		local CC = nil
 		local arenaFrame = _G["ArenaEnemyFrame"..i]
 		
 		if (arenaFrame["racial"] == nil) then
@@ -210,7 +210,7 @@ function module:OnEvent(event, ...)
 			CC:SetFrameLevel(4)
 
 			CC.cooldown:ClearAllPoints()
-			CC.cooldown:SetCooldownDuration(0)
+			CC.cooldown:SetCooldown(0, 0)
 			CC.cooldown:SetPoint("TOPLEFT", 1, -1)
 			CC.cooldown:SetPoint("BOTTOMRIGHT", -1, 1)
 		elseif event == "TEST_MODE" then
