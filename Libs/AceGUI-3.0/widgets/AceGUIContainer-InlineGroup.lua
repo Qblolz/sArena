fd5a00b6fd407d32e4ee277a6d52ddaed4c55311
Qblolz <doublemageinc@gmail.com>
2022-06-21 11:13:25 +0300

    update libs

--- Libs/AceGUI-3.0/widgets/AceGUIContainer-InlineGroup.lua
+++ Libs/AceGUI-3.0/widgets/AceGUIContainer-InlineGroup.lua
@@ -2,7 +2,7 @@
 InlineGroup Container
 Simple container widget that creates a visible "box" with an optional title.
 -------------------------------------------------------------------------------]]
-local Type, Version = "InlineGroup", 22
+local Type, Version = "InlineGroup", 20
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
@@ -19,7 +19,6 @@ local methods = {
 	["OnAcquire"] = function(self)
 		self:SetWidth(300)
 		self:SetHeight(100)
-		self:SetTitle("")
 	end,
 
 	-- ["OnRelease"] = nil,
@@ -75,7 +74,7 @@ local function Constructor()
 	titletext:SetJustifyH("LEFT")
 	titletext:SetHeight(18)
 
-	local border = CreateFrame("Frame", nil, frame, BackdropTemplateMixin and "BackdropTemplate" or nil)
+	local border = CreateFrame("Frame", nil, frame)
 	border:SetPoint("TOPLEFT", 0, -17)
 	border:SetPoint("BOTTOMRIGHT", -1, 3)
 	border:SetBackdrop(PaneBackdrop)
